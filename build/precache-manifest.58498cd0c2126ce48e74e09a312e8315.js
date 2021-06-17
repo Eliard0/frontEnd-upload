477deb1238f112be488f0e7d774db2f28b52a953
eliardo silva <eliardosilva81@gmail.com>
2021-06-17 17:33:58 -0300

    dnv

---
+++ build/precache-manifest.58498cd0c2126ce48e74e09a312e8315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd65e5868d1c05228243",
+    "url": "/static/js/main.cd65e586.chunk.js"
+  },
+  {
+    "revision": "cb2ee40ac53194b7cb00",
+    "url": "/static/css/1.3edebe13.chunk.css"
+  },
+  {
+    "revision": "cb2ee40ac53194b7cb00",
+    "url": "/static/js/1.cb2ee40a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b6d414495aa0b150897bd8c3943ae99",
+    "url": "/index.html"
+  }
+];
